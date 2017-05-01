85e7904d5082183d7332a9e21e35af1a8c2d4a8a
Jon Barksdale <jon.barksdale@gmail.com>
2017-04-30 18:25:04 -0700

    Updating install script to add symlinking.

--- install_homeshick.sh
+++ install_homeshick.sh
@@ -5,4 +5,17 @@
 
 base="${HOME}/.homesick/repos"
 
-git clone git://github.com/andsens/homeshick.git "${base}/homeshick"
+if [[ ! -e "${base}" ]]; then
+  echo "Cloning homeshick"
+  git clone git://github.com/andsens/homeshick.git "${base}/homeshick"
+fi
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+dotfiles_repo="$(dirname $(realpath $0))"
+
+if [[ ! -e "${base}/$(basename $dotfiles_repo)" ]]; then
+  (cd $base && ln -s $dotfiles_repo)
+fi
