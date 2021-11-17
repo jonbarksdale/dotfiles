3ff0f8bbb37cb1f877d8847172de491f872fe754
Jon Barksdale <jon.barksdale@gmail.com>
2021-11-16 18:09:38 -0700

    Emacs - add confluence exporter for org

--- emacs/config.el
+++ emacs/config.el
@@ -122,3 +122,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; enable confluence exporter for org
+(use-package! ox-confluence)
