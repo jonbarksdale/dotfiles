ff19f231ace0525c62dd99b77863678e8fa8ecb5
Jon Barksdale <jon.barksdale@gmail.com>
2021-11-29 16:10:02 -0700

    Emacs: add format & zen config
    How i missed format, I have no idea.

--- emacs/init.el
+++ emacs/init.el
@@ -49,13 +49,13 @@
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
