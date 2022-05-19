1cc7a09bf044470a725b5bb13215ded750a47286
Jon Barksdale <jon.barksdale@gmail.com>
2022-05-19 13:59:22 -0700

    Emacs: remove yes/no from TODO keywords
    I start a lot of notes with NO, as it turns out.

--- emacs/config.el
+++ emacs/config.el
@@ -40,6 +40,35 @@
 ;; May want to separate org dir?
 (setq org-directory (file-truename "~/org/"))
 
+(after! org
+  (setq org-todo-keywords
+        '((sequence
+           "TODO(t)"  ; A task that needs doing & is ready to do
+           "PROJ(p)"  ; A project, which usually contains other tasks
+           "LOOP(r)"  ; A recurring task
+           "STRT(s)"  ; A task that is in progress
+           "WAIT(w)"  ; Something external is holding up this task
+           "HOLD(h)"  ; This task is paused/on hold because of me
+           "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+           "|"
+           "DONE(d)"  ; Task successfully completed
+           "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+          (sequence
+           "[ ](T)"   ; A task that needs doing
+           "[-](S)"   ; Task is in progress
+           "[?](W)"   ; Task is being held up or paused
+           "|"
+           "[X](D)")  ; Task was completed
+          )
+        org-todo-keyword-faces
+        '(("[-]"  . +org-todo-active)
+          ("STRT" . +org-todo-active)
+          ("[?]"  . +org-todo-onhold)
+          ("WAIT" . +org-todo-onhold)
+          ("HOLD" . +org-todo-onhold)
+          ("PROJ" . +org-todo-project)
+          ("KILL" . +org-todo-cancel)))
+  )
 
 ;; org-roam config
 (setq org-roam-directory (file-truename "~/org/roam/"))
