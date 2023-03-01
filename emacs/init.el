245fa7062135b8b478bd40c076289d07afc517a1
Jon Barksdale <jon.barksdale@gmail.com>
2023-03-01 09:38:52 -0700

    emacs: remove forge
    Forge support seems to be broken at this time.  Also seems to slow down
    startup quite a bit.

--- emacs/init.el
+++ emacs/init.el
@@ -97,7 +97,7 @@
        gist              ; interacting with github gists
        (lookup +docsets +dictionary +offline)              ; navigate your code and its documentation
        lsp               ; M-x vscode
-       (magit +forge)             ; a git porcelain for Emacs
+       (magit)             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
