7230a04269a803fa27768297ac420d34226af3af
Jon Barksdale <jon.barksdale@gmail.com>
2023-08-16 10:18:52 -0700

    emacs: pin org commit that doesn't break roam
    Hopefully temporary - waiting on a fix from org that supports roam

--- emacs/packages.el
+++ emacs/packages.el
@@ -48,6 +48,8 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+;
+(package! org :pin "ca873f7")
 
 ;; github flavored markdown export from org
 (package! ox-gfm)
