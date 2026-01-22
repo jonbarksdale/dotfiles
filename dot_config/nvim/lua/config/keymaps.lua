559a676c5025beba8d9e6a784633103d6c272b5e
Jon Barksdale <jon.barksdale@gmail.com>
2026-01-21 21:54:00 -0700

    docs(nvim): document leader-m as mode-specific prefix
    Establishes <leader>m convention for filetype-specific bindings,
    following Doom Emacs pattern. Plugins use <leader>m* with ft=
    restrictions for buffer-local behavior.

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,14 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- ABOUTME: Custom keymaps extending LazyVim defaults
+-- ABOUTME: Establishes <leader>m as mode-specific prefix (like Doom Emacs)
+
+-- Default keymaps: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 --
+-- Keymap conventions:
+--   <leader>m  - Mode/filetype-specific commands (markdown tables, LSP actions, etc.)
+--   <leader>M  - Metals (Scala LSP) - moved from <leader>m
+--
+-- File-type plugins should define <leader>m* bindings via autocmds or ft= in lazy.nvim
+
 local map = vim.keymap.set
 
 map("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
