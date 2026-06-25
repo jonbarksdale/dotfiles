6875f94afd333c7827426b9a16c81a814e7a4fe0
Jon Barksdale <jon.barksdale@gmail.com>
2026-06-25 11:14:04 -0700

    chore(doom): switch org to roam v1, add dragndrop module

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -154,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        (ocaml +tree-sitter) ; an objective camel
-       (org +roam2 +pandoc +pretty +present +jupyter)               ; organize your plain life in plain text
+       (org +roam +pandoc +pretty +present +jupyter +dragndrop)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
